bf7360e04975470b26db65874069416815207115
smboolean76 <smboolean76@madrigali.dev>
2022-10-24 13:00:10 +0200

    add: creation board by JS

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 "use strict";
 
+// Creation Board
+const boardContainer = document.querySelector('.board');
+const maxNumber = 20;
+
+for( let i = 1; i <= maxNumber; i++ ) {
+    const boardNumber = document.createElement('div');
+    boardNumber.classList.add('board__number');
+    boardNumber.innerHTML = i;
+    boardContainer.append(boardNumber);
+}
+
 const btnGenerator = document.getElementById("btn-generator");
 const generatorNumberElement = document.querySelector(".generator__number");
 
 btnGenerator.addEventListener('click', function() {
-    const numerRandom = Math.floor(Math.random() * 10) + 1;
+    const numerRandom = Math.floor(Math.random() * maxNumber) + 1;
     const cellBoard = document.querySelector(`.board__number:nth-child(${numerRandom})`);
     generatorNumberElement.innerHTML = numerRandom;
 
